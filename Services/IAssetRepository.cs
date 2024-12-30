3750a8c4713f65b28f682e596ed394b9b978be2a
henriquedeantoni <henriqdeantoni@gmail.com>
2024-12-29 21:57:38 -0300

    feat: created interface asset

---
+++ Services/IAssetRepository.cs
@@ -0,0 +1,21 @@
+﻿using AssetManager.Entities;
+
+namespace AssetManager.Services
+{
+    public interface IAssetRepository
+    {
+        Task<IEnumerable<Asset>> GetAssetsAsync();
+
+        Task<IEnumerable<Asset>> GetAllAssetsAsync();
+
+        Task<IEnumerable<Asset>> GetAsset(string assetId);
+
+        void CreateAsset(Asset asset);
+
+        void UpdateAsset(Guid id, Asset asset);
+
+        void DeleteAsset(Guid id);
+
+        Task<bool> SaveChangesAsync();
+    }
+}
