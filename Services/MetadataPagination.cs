b21b6a99c57b952014bd81d34c561c75c9f89b17
henriquedeantoni <henriqdeantoni@gmail.com>
2025-01-02 22:40:56 -0300

    feat: introduced metada for pagination

---
+++ Services/MetadataPagination.cs
@@ -0,0 +1,18 @@
+﻿namespace AssetManager.Services
+{
+    public class MetadataPagination
+    {
+        public int TotalItemCount { get; set; }
+        public int TotalPageCount { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+
+        public MetadataPagination(int totalItemCount, int currentPage, int pageSize)
+        {
+            TotalItemCount = totalItemCount;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalPageCount = (int)Math.Ceiling(totalItemCount / (double)pageSize);
+        }   
+    }
+}
